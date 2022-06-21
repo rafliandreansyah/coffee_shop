59986ac22264c49f8ced040363e9c55886aec16c
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2022-06-21 20:01:58 +0700

    upgrade flutter

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Submission 1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
